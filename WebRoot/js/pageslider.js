703a54c80a9d7a3e929735eaf4307927bea64e2a
tony60107 <tony60107@yahoo.com.tw>
2016-10-11 22:05:24 +0800

    完成底部頁面縮圖滾動功能

--- WebRoot/js/pageslider.js
+++ WebRoot/js/pageslider.js
@@ -6,12 +6,24 @@
 var PageSlider = Class.extend({
 
     init: function(){
+        var pageList = $('#pageList');
 
     },
 
     bindEvent: function(){
-        $('prevPages').onclick = function(){
-
-        }
+        $('#prevPages').on('click', function(){
+            if(pageList.offsetLeft < pageList.parentNode.offsetWidth - 162 * pageList.children.length + 400){
+                animate(pageList, pageList.parentNode.offsetWidth - 162 * pageList.children.length - 10);
+                return;
+            }
+            animate(pageList, pageList.offsetLeft-400);
+        });
+        $('#nextPages').on('click', function(){
+            if(pageList.offsetLeft >= 0  - 400){
+                animate(pageList, 0);
+                return;
+            }
+            animate(pageList, pageList.offsetLeft+400);
+        });
     },
 });
